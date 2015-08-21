2ad16948248b5914f8ab8bab608787791fc9a365
Jeremy Yap <jeruyyap@gmail.com>
2015-08-20 22:43:51 -0400

    Added opencv_snippets/simple_balltracker

---
+++ opencv_snippets/simple_balltracker/README.md
@@ -0,0 +1,24 @@
+# Simple OpenCV Ball Tracker
+
+A simple OpenCV ball tracker in Python.
+
+## Dependencies
+
+- Python 2.7 
+- OpenCV with Python bindings http://opencv.org/ 
+
+## Usage
+
+Have a webcam connected and run balltracker.py
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## Credits
+
+Jeremy Yap
---
+++ opencv_snippets/simple_balltracker/balltracker.py
@@ -0,0 +1,40 @@
+__author__ = 'jeremy'
+
+import cv2
+import cv2.cv as cv
+
+
+def main():
+    # capture video from web camera
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # get frame from camera
+        ret, frame = cap.read()
+
+        # get grayscale image from frame
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # get list of circles using HoughCircles function. See OpenCV documentation for details.
+        # these parameters will need a lot of tweaking.
+        circles = cv2.HoughCircles(gray_frame, cv.CV_HOUGH_GRADIENT, 1, 20, param1=40, param2=40, minRadius=10, maxRadius=40)
+
+        # mark the found circles with  visiblecircles on the frame.
+        try:
+            l = len(circles[0,:])
+            for i in circles[0,:]:
+                cv2.circle(frame,(i[0],i[1]),15,(0,0,255),5)
+        except TypeError:
+            pass
+
+        # show the frame
+        cv2.imshow('ball tracker', frame)
+        cv2.waitKey(1)
+
+    # remove window
+    cv2.destroyAllWindows()
+
+
+
+if __name__=='__main__':
+    main()
